12cee3e64e5960d424867a1fee5253c81c57f979
Opetunde <dhorllyibi@gmail.com>
2022-03-12 10:22:22 +0100

    commiting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,8 +19,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-}sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
 }
