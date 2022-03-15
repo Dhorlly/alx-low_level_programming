bb4705a04bef963d458b2e8e07100fe3a1a61573
Kay <kay@Kays-MBP.lan>
2022-03-15 14:13:27 -0700

    committing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
+#include <stdio>
 #include <unistd.h>
-
 /**
 * file:0-putchar.c
 * main - prints -putchar, followed by a new line
 */
 int main(void)
 {
-	_putchar("_putchar");
-	_putchar(" ");
+	_putchar('_putchar');
+	_putchar('\n');
+
 	return (0);
 }
