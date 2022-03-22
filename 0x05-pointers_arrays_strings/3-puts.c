dad911ed74599d32a8440748cd54775ae5d40219
Opetunde <dhorllyibi@gmail.com>
2022-03-22 03:12:28 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *   _puts - check the code for ALX  students.
+ *   @str: variable.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
