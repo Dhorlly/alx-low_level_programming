e1f41e1834ecb1f5b82c332cdf416e17a0f2b835
Opetunde <dhorllyibi@gmail.com>
2022-03-22 23:07:18 +0200

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,6 @@
  */
 void print_rev(char *s)
 {
-		int l;
-
-			l = 0;
-				while (*(s + l) != '\0')
-							l++;
-					l--;
-						for (; l >= 0; l--)
-									_putchar(*(s + l));
-							_putchar('\n');
+	char l=;
+	strrev(l);
 }
