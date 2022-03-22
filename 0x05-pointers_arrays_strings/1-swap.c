1d506cf9f02cfb08cad13f50a8750e8af2ba8c8f
Opetunde <dhorllyibi@gmail.com>
2022-03-22 02:33:28 +0200

    commiting

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * void swap_int(int *a, int *b); :function that swaps the values of two integers
+ * @a : first integer
+ * @b : second integer
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a=*b;
+}
