1d31dd9ad83d1c98f7e6afa0a8b78e7b1f58568c
Opetunde <dhorllyibi@gmail.com>
2022-03-22 20:32:56 +0200

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen - the function returns the length of a string
- * @s: pointer
- * Return: the length of a string
+ *_strlen - the function returns the length of a string
+ *@s: pointer
+ *Return: the length of a string
  */
 int _strlen(char *s)
 {
