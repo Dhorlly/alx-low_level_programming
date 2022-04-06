982c96befa4be89237c3a4cfcfe6008a41a47b4d
Opetunde <dhorllyibi@gmail.com>
2022-04-06 23:18:44 +0300

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,19 @@
 /**
 * file:0-putchar.c
 * main - prints -putchar, followed by a new line
+*
+* Retrun: 0 (Success)
 */
 int _putchar(void)
 {
-	_putchar('_putchar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
