8578ffe31982cb76b808d42a514ade9a6de9fb68
Opetunde <dhorllyibi@gmail.com>
2022-04-11 22:58:50 +0300

    CAC

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
-* File: dog.h
-* Desciption: file that declares three types of struct dog.
-*/
-
 /**
 * struct dog - declaring of struct dog
 * @name: First member
